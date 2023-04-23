1b8a6ef0fa498c8e4ffaa8d87c01168cee2ef66f
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2023-04-23 13:05:59 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@
 {
     public static class ModHelperData
     {
-        public const string WorksOnVersion = "34.3";
+        public const string WorksOnVersion = "36.3";
         public const string Version = "1.0.3";
         public const string Name = "Paragon Totem In Shop";
         public const string Description =
